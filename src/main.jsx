af571d6091b5b336de3f883f40a2a8763b7f0ad0
JDM297 <j.d.miller297@gmail.com>
2024-07-20 17:12:03 -0400

    Fixing issues with GH Pages not playing nice with Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
+import App from './App.jsx';
 import './styles/tailwind.css';
+
+const basename = process.env.NODE_ENV === 'production' ? '/valor-career-website' : '/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
